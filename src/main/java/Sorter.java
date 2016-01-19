e50941d9ed9e5a6bd8a7fdac38156f5f95f75af1
joseph <joseph@Zipcoder06s-MacBook-Pro.local>
2016-01-19 16:54:28 -0500

    Add Sorter class.

---
+++ src/main/java/Sorter.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * Created by joseph on 1/19/16.
+ */
+public class Sorter {
+    public Sorter() {
+
+    }
+
+    public void sort() {
+
+    }
+
+    public ArrayList<String> getListOfWords() {
+        return null;
+    }
+    
+    private ArrayList<String> listOfWords;
+}
